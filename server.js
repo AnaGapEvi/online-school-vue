d4bfca79146b1342ed985d2541b9c5e3901719e4
Garegin <garegin.makhmuryan@evollu.com>
2022-08-30 13:06:07 +0400

    server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-// const port = process.env.PORT || 3000
-
-const serveStatic = require("serve-static")
-const path = require('path');
-
+const port = process.env.PORT || 8080
 app = express();
+app.use(express.static(path.join(__dirname + "/dist/")));
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
-app.listen(process.env.PORT || 3000, function(){
+// const serveStatic = require("serve-static")
+// const path = require('path');
+
+
+
+app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
