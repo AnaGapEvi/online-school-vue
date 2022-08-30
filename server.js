99865692d357e26307593c11e4d8a4d2f3736e88
Garegin <garegin.makhmuryan@evollu.com>
2022-08-30 15:45:48 +0400

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const port = process.env.PORT || 3000;
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
 
-const app = express();
+const app = express()
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/ , function(req,res) {
-  res.sendfile(__dirname + "/dist/index.html");
-});
-
-app.listen(port);
-
-console.log("Server started...");
+app.use(history())
+app.use(serveStatic(__dirname + '/dist/spa'))
+app.listen(port)
